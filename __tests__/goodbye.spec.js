59244bb4c43dcbdb05c738c8af9aabe96df26bf3
xJkit <xjkit54780@gmail.com>
2018-04-16 19:34:08 +0800

    Add test cases for Provider

--- __tests__/goodbye.spec.js
+++ __tests__/goodbye.spec.js
@@ -3,11 +3,46 @@ import { shallow } from 'enzyme';
 
 import { Provider } from '../src';
 
-test('<Provider /> exists', () => {
-  const wrapper = shallow(
-    <Provider>
-      {() => {}}
-    </Provider>
-  );
-  expect(wrapper).toBeTruthy();
-});
+describe('Provider', () => {
+  test('should render correctly', () => {
+    const wrapper = shallow(
+      <Provider>
+        {() => {}}
+      </Provider>
+    );
+    expect(wrapper).toBeTruthy();
+  });
+
+  test('should receives handleGetUserConfirm props', () => {
+    shallow(
+      <Provider>
+        {(props) => {
+          expect(props.handleGetUserConfirm).toBeTruthy();
+        }}
+      </Provider>
+    )
+  });
+
+  test('should have initial state isShow to be false', () => {
+    const wrapper = shallow(
+      <Provider>
+        {() => {}}
+      </Provider>
+    );
+    expect(wrapper.state('isShow')).toBe(false);
+  })
+
+  test('should have isShow to be true after handleGetUserConfirm function call', () => {
+    let handleFunc;
+    const wrapper = shallow(
+      <Provider>
+        {({ handleGetUserConfirm }) => {
+          handleFunc = handleGetUserConfirm;
+        }}
+      </Provider>
+    );
+    handleFunc();
+    wrapper.update();
+    expect(wrapper.state('isShow')).toBe(true);
+  })
+})
